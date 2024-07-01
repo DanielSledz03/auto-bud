dfe63cfaab0529b5e47aba6a8f21fb0bffb4eadb
DanielSledz03 <daniel.sledz@pluscode.io>
2024-07-01 21:01:56 +0200

    add: about us section in homepage

--- src/view/HomeAboutUs/HomeAboutUs.tsx
+++ src/view/HomeAboutUs/HomeAboutUs.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 
-import CarWash from '@/../public/assets/images/general/index/aboutCarWash.png';
-import StationImage from '@/../public/assets/images/general/index/aboutStation.png';
 import BackgroundImage from '@/../public/assets/images/general/index/aboutUsBackground.png';
+import AboutUsImages from '@/../public/assets/images/general/index/aboutUsImages.png';
+import AboutUsImagesDesktop from '@/../public/assets/images/general/index/aboutUsImagesDesktop.png';
 import WhiteButton from '@/components/WhiteButton/WhiteButton';
 
 import styles from './HomeAboutUs.module.scss';
@@ -17,46 +17,39 @@ const HomeAboutUs = () => {
       />
 
       <div className={styles.aboutUs__content}>
-        <h3 className={styles.aboutUs__title}>O nas</h3>
+        <div className={styles.aboutUs__text__wrapper}>
+          <h3 className={styles.aboutUs__title}>O nas</h3>
 
-        <hr className={styles.aboutUs__line} />
+          <hr className={styles.aboutUs__line} />
 
-        <p className={styles.aboutUs__text__bold}>
-          Grupa AUTO-BUD to kompleks obiektów motoryzacyjnych mieszczących się w
-          Rudzie Śląskiej i Świętochłowicach.
-        </p>
+          <p className={styles.aboutUs__text__bold}>
+            Grupa AUTO-BUD to kompleks obiektów motoryzacyjnych mieszczących się
+            w Rudzie Śląskiej i Świętochłowicach.
+          </p>
 
-        <p className={styles.aboutUs__text}>
-          Jako pierwsza powstała okręgowa Stacja Kontroli Pojazdów Omega, która
-          już od 8 lat sprawdza sprawność techniczną rudzkich pojazdów. W
-          późniejszych latach obiekt na Goduli rozwinął się o stację LPG oraz
-          mały warsztat samochodowy specjalizujący się w zakładaniu i
-          serwisowaniu instalacji LPG.
-        </p>
+          <p className={styles.aboutUs__text}>
+            Jako pierwsza powstała okręgowa Stacja Kontroli Pojazdów Omega,
+            która już od 8 lat sprawdza sprawność techniczną rudzkich pojazdów.
+            W późniejszych latach obiekt na Goduli rozwinął się o stację LPG
+            oraz mały warsztat samochodowy specjalizujący się w zakładaniu i
+            serwisowaniu instalacji LPG.
+          </p>
 
-        <WhiteButton title="Czytaj więcej" />
+          <WhiteButton title="Czytaj więcej" />
+        </div>
 
         <div className={styles.aboutUs__images}>
-          <div className={styles.aboutUs__image__wrapper}>
-            <p className={`${styles.aboutUs__image__title}`}>
-              Okręgowa Stacja Kontroli Pojazdów
-            </p>
-            <Image
-              className={styles.aboutUs__image}
-              src={StationImage}
-              alt="Stacja Diagnostyczna Świętochłowice"
-            />
-          </div>
-          <div className={styles.aboutUs__image__wrapper}>
-            <p className={styles.aboutUs__image__title__second}>
-              Myjnia Bezdotykowa
-            </p>
-            <Image
-              className={styles.aboutUs__image__second}
-              src={CarWash}
-              alt="Myjnia samochodowa"
-            />
-          </div>
+          <Image
+            src={AboutUsImages}
+            className={styles.aboutUs__images__image}
+            alt="Stacja Diagnostyczna oraz Myjnia Samochodowa"
+          />
+
+          <Image
+            src={AboutUsImagesDesktop}
+            className={styles.aboutUs__images__image__desktop}
+            alt="Stacja Diagnostyczna oraz Myjnia Samochodowa"
+          />
         </div>
       </div>
     </div>
