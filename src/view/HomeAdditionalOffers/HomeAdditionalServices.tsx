b8ccc90828fd6a10ed19c743e1a0f872ca24c7cc
DanielSledz03 <daniel.sledz@pluscode.io>
2024-07-05 16:53:21 +0200

    edit: prettier config, styles

---
+++ src/view/HomeAdditionalOffers/HomeAdditionalServices.tsx
@@ -0,0 +1,11 @@
+import styles from './HomeAdditionalServices.module.scss';
+
+const HomeAdditionalServices = () => {
+  return (
+    <div className={styles.container}>
+      <h1>HomeAdditionalServices</h1>
+    </div>
+  );
+};
+
+export default HomeAdditionalServices;
