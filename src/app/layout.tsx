5abe15d0b773e29ea64e9030bb92a1a8e0622093
DanielSledz03 <daniel.sledz@pluscode.io>
2024-07-19 22:22:37 +0200

    edit: env name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,9 @@ export default function RootLayout({
         </main>
         <Footer />
       </body>
-      <GoogleAnalytics gaId={process.env.GOOGLE_TAG_MANAGER_ID || ''} />
+      <GoogleAnalytics
+        gaId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || ''}
+      />
     </html>
   );
 }
