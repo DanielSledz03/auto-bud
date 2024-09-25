bc81647bc26ef14b25347c0695e3e10bdafa9f2a
DanielSledz03 <daniel.sledz@pluscode.io>
2024-09-25 16:57:53 +0200

    fix: choose the time component

--- src/view/ChooseTheTime/ChooseTheTime.tsx
+++ src/view/ChooseTheTime/ChooseTheTime.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useController, Control, FieldValues, Path } from 'react-hook-form';
 
 import RedArrow from '@/icons/redArrowDown.svg'; // Strzałka w prawo
@@ -36,17 +36,13 @@ interface DatePickerProps<T extends FieldValues> {
   name: Path<T>;
   control: Control<T>;
 }
-
 const ChooseTheTime = <T extends FieldValues>({
   name,
   control,
 }: DatePickerProps<T>) => {
-  const [startDate, setStartDate] = useState(() => {
-    const date = new Date();
-    date.setDate(date.getDate() + 1);
-    return date;
-  });
-  const [daysToShow, setDaysToShow] = useState(0); // Domyślna liczba dni do wyświetlenia na mobilnych
+  const [startDate, setStartDate] = useState(() => new Date());
+  const [daysToDisplay, setDaysToDisplay] = useState(7); // Domyślnie 7 dni
+  const currentYear = new Date().getFullYear();
 
   const {
     field: { value, onChange },
@@ -55,120 +51,100 @@ const ChooseTheTime = <T extends FieldValues>({
     control,
   });
 
-  const getDate = (daysToAdd: number) => {
-    const date = new Date(startDate);
-    date.setDate(date.getDate() + daysToAdd);
-    return date;
-  };
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 768) {
+        setDaysToDisplay(180); // Mobile
+      } else {
+        setDaysToDisplay(7); // Desktop
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Ustawienie początkowej wartości na podstawie bieżącej szerokości
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const formatDate = (date: Date) => {
-    const day = date.toLocaleDateString('pl-PL', {
-      day: 'numeric',
-    });
-
-    const dayOfWeek = date.toLocaleDateString('pl-PL', {
-      weekday: 'short',
-    });
-
+    const day = date.toLocaleDateString('pl-PL', { day: 'numeric' });
+    const dayOfWeek = date.toLocaleDateString('pl-PL', { weekday: 'short' });
     const month = date
-      .toLocaleDateString('pl-PL', {
-        month: 'long',
-      })
+      .toLocaleDateString('pl-PL', { month: 'long' })
       .replace(/^\p{L}/u, c => c.toUpperCase());
 
-    return {
-      day,
-      dayOfWeek,
-      month,
-    };
+    return { day, dayOfWeek, month };
   };
 
-  const isWeekend = (date: Date) => {
-    const dayOfWeek = date.getDay();
-    return dayOfWeek === 0 || dayOfWeek === 6; // Niedziela = 0, Sobota = 6
-  };
+  const isWeekend = (date: Date) => date.getDay() === 0 || date.getDay() === 6;
 
   const isHoliday = (date: Date) => {
     const formattedDate = date.toISOString().split('T')[0];
     return holidays.includes(formattedDate);
   };
 
+  const getNextBusinessDay = (currentDate: Date) => {
+    const nextDate = new Date(currentDate);
+    nextDate.setDate(currentDate.getDate() + 1);
+    while (isWeekend(nextDate) || isHoliday(nextDate)) {
+      nextDate.setDate(nextDate.getDate() + 1);
+    }
+    return nextDate;
+  };
+
   const handlePrevWeek = () => {
     setStartDate(prevDate => {
-      const newDate = new Date(prevDate);
-      newDate.setDate(newDate.getDate() - 10);
-      return newDate;
+      let newDate = new Date(prevDate);
+      const today = new Date();
+      today.setHours(0, 0, 0, 0); // Reset czasu do początku dnia
+      for (let i = 0; i < daysToDisplay; i++) {
+        newDate = getNextBusinessDay(newDate);
+      }
+      return newDate < today ? today : newDate;
     });
   };
 
   const handleNextWeek = () => {
     setStartDate(prevDate => {
-      const newDate = new Date(prevDate);
-      newDate.setDate(newDate.getDate() + 10);
+      let newDate = new Date(prevDate);
+      for (let i = 0; i < daysToDisplay; i++) {
+        newDate = getNextBusinessDay(newDate);
+      }
       return newDate;
     });
   };
 
-  useEffect(() => {
-    const updateDaysToShow = () => {
-      if (window.innerWidth >= 1024) {
-        setDaysToShow(10); // Pokaż 7 dni na większych ekranach
-      } else {
-        setDaysToShow(180); // Pokaż 180 dni na mobilnych
-      }
-    };
-
-    updateDaysToShow();
-    window.addEventListener('resize', updateDaysToShow);
-
-    return () => {
-      window.removeEventListener('resize', updateDaysToShow);
-    };
-  }, []);
-
   const buttons = [];
-  let index = 0;
-  const currentMonth = startDate.getMonth();
+  let currentDate = new Date(startDate);
 
-  while (index < daysToShow) {
-    const date = getDate(index);
-    if (date.getMonth() !== currentMonth) {
-      break;
-    }
-
-    const formattedDate = formatDate(date);
+  for (let i = 0; i < daysToDisplay; i++) {
+    const buttonDate = new Date(currentDate);
+    currentDate = getNextBusinessDay(currentDate);
+    const formattedDate = formatDate(buttonDate);
     const isSelected =
-      value && new Date(value).toDateString() === date.toDateString();
-
-    if (isWeekend(date) || isHoliday(date)) {
-      index++;
-      continue;
-    }
+      value && new Date(value).toDateString() === buttonDate.toDateString();
 
     buttons.push(
       <button
         type="button"
-        key={index}
-        onClick={() => onChange(date.toISOString())}
+        key={buttonDate.toISOString()}
+        onClick={() => onChange(buttonDate.toISOString())}
         className={`${styles.datePicker__day} ${
           isSelected ? styles.datePicker__daySelected : ''
-        } ${index === 0 ? styles.datePicker__day__first : ''}`}
+        } ${i === 0 ? styles.datePicker__day__first : ''}`}
       >
         <span className={styles.datePicker__dayOfWeek}>
           {formattedDate.dayOfWeek.replace('.', '')}
         </span>
-
         <span className={styles.datePicker__dayNumber}>
           {formattedDate.day}
         </span>
-
-        <span className={styles.datePicker__monthMobile}>
-          {formattedDate.month}
-        </span>
+        <span className={styles.datePicker__month}>{formattedDate.month}</span>
       </button>,
     );
-
-    index++;
   }
 
   return (
@@ -186,9 +162,7 @@ const ChooseTheTime = <T extends FieldValues>({
             alt="Strzałka"
           />
         </button>
-
         {buttons}
-
         <button
           type="button"
           onClick={handleNextWeek}
@@ -201,14 +175,11 @@ const ChooseTheTime = <T extends FieldValues>({
           />
         </button>
       </div>
-      <div className={styles.datePicker__month}>
-        {startDate
-          .toLocaleDateString('pl-PL', {
-            month: 'long',
-            year: 'numeric',
-          })
-          .replace(/^\p{L}/u, c => c.toUpperCase())}
-      </div>
+      {startDate.getFullYear() !== currentYear && (
+        <div className={styles.datePicker__month}>
+          {startDate.getFullYear()}
+        </div>
+      )}
     </div>
   );
 };
